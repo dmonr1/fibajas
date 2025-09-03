3e39e2423438377ebfd4778faa4d64584b60f6dd
diegor <diegorutti26@gmail.com>
2025-09-03 15:22:56 -0500

    buttons

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [NzAvatarModule],
   templateUrl: './main.html',
   standalone: true,
   styleUrl: './main.scss'
@@ -11,7 +14,13 @@ export class Main {
 
   activeBtn: string | null = null;
 
-
+  usuario = {
+    inicial: 'U',
+    nombres: 'Juan',
+    apellidos: 'Pérez Gómez',
+    oficina: 'Central',
+    area: 'Recursos Humanos'
+  };
   toggleLabel(btn: string) {
     this.activeBtn = this.activeBtn === btn ? null : btn;
   }
